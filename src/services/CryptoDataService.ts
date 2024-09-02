f2ce7dbd7b261556deb913044b1e10521ea087a7
Manuel Garrido <m.garrido@quix.com.do>
2024-09-01 22:05:59 -0400

    Update getCryptoData by using coinmarketcap

--- src/services/CryptoDataService.ts
+++ src/services/CryptoDataService.ts
@@ -1,19 +1,52 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
-const COINGECKO_API_URL = "https://api.coingecko.com/api/v3/simple/price";
+const COINMARKETCAP_API_URL =
+  "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
+const API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 const getCryptoData = async (symbols: string[]) => {
   try {
-    const response = await axios.get(COINGECKO_API_URL, {
+    const upperCasedSymbols = symbols.map((symbol) => symbol.toUpperCase());
+    const response = await axios.get(COINMARKETCAP_API_URL, {
       params: {
-        ids: symbols.join(","),
-        vs_currencies: "usd",
+        symbol: upperCasedSymbols.join(","),
+        convert: "USD", // Convert the prices to USD
+      },
+      headers: {
+        "X-CMC_PRO_API_KEY": API_KEY, // Set the CoinMarketCap API key
       },
     });
 
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching cryptocurrency data:", error);
+    const coinData: {
+      [key: string]: {
+        usd: number;
+        market_cap: number;
+        percent_change_24h: number;
+      };
+    } = {};
+    const data = response.data.data;
+
+    upperCasedSymbols.forEach((symbol) => {
+      if (data[symbol]) {
+        coinData[symbol.toLowerCase()] = {
+          usd: data[symbol].quote.USD.price,
+          market_cap: data[symbol].quote.USD.market_cap,
+          percent_change_24h: data[symbol].quote.USD.percent_change_24h,
+        };
+      }
+    });
+
+    return coinData;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.error("Axios error:", error.response?.data || error.message);
+    } else if (error instanceof Error) {
+      console.error("Error:", error.message);
+    } else {
+      console.error("Unknown error:", error);
+    }
     throw new Error("Failed to fetch cryptocurrency data");
   }
 };
